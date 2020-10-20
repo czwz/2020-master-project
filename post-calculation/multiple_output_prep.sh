f03b403733dfe973eff9b348bd662b86311c37d2
slwang <33646491+tim010007@users.noreply.github.com>
2020-10-20 22:48:34 +0200

    update script including optimized code for nc

--- post-calculation/multiple_output_prep.sh
+++ post-calculation/multiple_output_prep.sh
@@ -1,37 +1,45 @@
-if ! [ -d processed_data ]; then
-	mkdir processed_data;
-	cd processed_data;
+if ! [ -d processed_output ]; then
+	mkdir processed_output;
+	cd processed_output;
 
 	mkdir trj;
-	#for r in $(seq 1 1 5); do
-	for r in $(seq 1 1 2); do
+	for r in $(seq 1 1 5); do
 
-		echo "processing sample $r..."
+		echo "processing sample $r..." >> process.log;
                 mkdir trj/sample-$r;
 
-		#for filename in $(ls /scratch/snx3000/swang/md-input); do
-		for filename in "3E2H_b_1" "3E2H_g_1"; do
+		for filename in $(ls /scratch/snx3000/swang/md-input); do
 
-			echo "processing $filename ...";
+			echo "processing $filename ..." >> process.log;
 			mkdir trj/sample-$r/$filename;
 			cp /scratch/snx3000/swang/md-post/tool/single_output_prep.sh -t /scratch/snx3000/swang/md-run/sample-$r/$filename;
+			cp /scratch/snx3000/swang/md-post/tool/VMDextensions.tcl -t /scratch/snx3000/swang/md-run/sample-$r/$filename;
 
 			cd /scratch/snx3000/swang/md-run/sample-$r/$filename;
 			bash /scratch/snx3000/swang/md-run/sample-$r/$filename/single_output_prep.sh $filename;
-			cd -;
+			cd /scratch/snx3000/swang/md-post/processed_output;
 
-			mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.nc r_${r}_$filename.nc;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.pnc r_${r}_$filename.pnc;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.enc r_${r}_$filename.enc;
                         mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.rgyr r_${r}_$filename.rgyr;
-			mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.rmsf r_${r}_$filename.rmsf;
-			mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.ermsd r_${r}_$filename.ermsd;
-			mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.prmsd r_${r}_$filename.prmsd;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.rmsf r_${r}_$filename.rmsf;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.pprmsd r_${r}_$filename.pprmsd;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.eprmsd r_${r}_$filename.eprmsd;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.sprmsd r_${r}_$filename.sprmsd;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.eermsd r_${r}_$filename.eermsd;
+                        mv /scratch/snx3000/swang/md-run/sample-$r/$filename/$filename.sermsd r_${r}_$filename.sermsd;
+
 			cp /scratch/snx3000/swang/md-run/sample-$r/$filename/step3_charmm2gmx.pdb -t trj/sample-$r/$filename;
 			mv /scratch/snx3000/swang/md-run/sample-$r/$filename/*noPBC.xtc -t trj/sample-$r/$filename;
 
 			rm /scratch/snx3000/swang/md-run/sample-$r/$filename/view_state.vmd;
+                        rm /scratch/snx3000/swang/md-run/sample-$r/$filename/vmd.log;
 			rm /scratch/snx3000/swang/md-run/sample-$r/$filename/vmd.tcl;
 			rm /scratch/snx3000/swang/md-run/sample-$r/$filename/single_output_prep.sh;
+			rm /scratch/snx3000/swang/md-run/sample-$r/$filename/VMDextensions.tcl;
+
 		done;
 	done;
+	echo "done." >> process.log;
 	cd ../;
 fi
--- post-calculation/single_output_prep.sh
+++ post-calculation/single_output_prep.sh
@@ -1,10 +1,15 @@
 module load daint-gpu
 module load GROMACS
 
-echo "0" | gmx trjconv -s step4.1_equilibration.tpr -f step4.1_equilibration.xtc -o step4.1_equilibration-noPBC.xtc -pbc mol -ur compact
-echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.xtc -o step5_1-noPBC.xtc -pbc mol -ur compact
-echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0002.xtc -o step5_1.part0002-noPBC.xtc -pbc mol -ur compact
-echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0003.xtc -o step5_1.part0003-noPBC.xtc -pbc mol -ur compact
+echo "0" | gmx trjconv -s step4.1_equilibration.tpr -f step4.1_equilibration.xtc -o step4.1_equilibration-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.xtc -o step5_1-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0002.xtc -o step5_1.part0002-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0003.xtc -o step5_1.part0003-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0004.xtc -o step5_1.part0004-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0005.xtc -o step5_1.part0005-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0006.xtc -o step5_1.part0006-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0007.xtc -o step5_1.part0007-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
+echo "0" | gmx trjconv -s step5_1.tpr -f step5_1.part0008.xtc -o step5_1.part0008-noPBC.xtc -pbc mol -ur compact >> vmd.log 2>&1
 
 NAME=$1
 VMD_EXE='/users/swang/program/vmd-1.9.4a43/bin/vmd'
@@ -29,12 +34,17 @@ mol addfile step4.1_equilibration-noPBC.xtc type xtc first 0 last -1 step 20 fil
 mol addfile step5_1-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
 mol addfile step5_1.part0002-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
 mol addfile step5_1.part0003-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol addfile step5_1.part0004-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol addfile step5_1.part0005-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol addfile step5_1.part0006-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol addfile step5_1.part0007-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol addfile step5_1.part0008-noPBC.xtc type xtc first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
 
 #################################
 #clip till max duration and save#
 #################################
 
-animate delete beg 5000 end 10000 skip -1
+animate delete beg 20000 end 99999 skip -1
 save_state view_state.vmd
 
 ############################
@@ -46,44 +56,59 @@ set sel [atomselect top "protein and name CA"]
 set all [atomselect top "all"]
 set n [molinfo top get numframes]
 
-for {set i 1} {\$i < \$n} {incr i} {
+for {set i 0} {\$i < \$n} {incr i} {
     \$sel frame \$i   
     \$all frame \$i
     \$all move [measure fit \$sel \$ref]
     }
 
-##########################################################
-#calculate & output protein CA rmsd aligned w.r.t CA(t=0)#
-##########################################################
+#################################################################
+#prmsd: calculate & output overall CA rmsd aligned w.r.t CA(t=0)#
+#################################################################
 
-set outfile [open "$NAME.prmsd" w]
+set outfile [open "$NAME.pprmsd" w]
 set ref [atomselect top "protein and name CA" frame 0]
 set sel [atomselect top "protein and name CA"]
 set n [molinfo top get numframes]
-for {set i 1} {\$i < \$n} {incr i} {
+for {set i 0} {\$i < \$n} {incr i} {
   \$sel frame \$i
   set rmsd [measure rmsd \$sel \$ref]
   puts \$outfile "\$i \$rmsd"
   }
 close \$outfile
 
-##########################################################
-#calculate & output epitope CA rmsd aligned w.r.t CA(t=0)#
-##########################################################
+##############################################################################
+#prmsd-side: calculate & output overall side chain rmsd aligned w.r.t CA(t=0)#
+##############################################################################
 
-set outfile [open "$NAME.ermsd" w]
+set outfile [open "$NAME.sprmsd" w]
+set ref [atomselect top "protein and noh and not name CA" frame 0]
+set sel [atomselect top "protein and noh and not name CA"]
+set n [molinfo top get numframes]
+for {set i 0} {\$i < \$n} {incr i} {
+  \$sel frame \$i
+  set rmsd [measure rmsd \$sel \$ref]
+  puts \$outfile "\$i \$rmsd"
+  }
+close \$outfile
+
+##################################################################
+#ermsd: calculate & output epitope CA rmsd aligned w.r.t  CA(t=0)#
+##################################################################
+
+set outfile [open "$NAME.eprmsd" w]
 set ref [atomselect top "protein and name CA and (resid $epitope)" frame 0]
 set sel [atomselect top "protein and name CA and (resid $epitope)"]
 for {set i 0} {\$i < \$n} {incr i} {
   \$sel frame \$i
   set rmsd [measure rmsd \$sel \$ref]
   puts \$outfile "\$i \$rmsd"
-}
+  }
 close \$outfile
 
-##################################################
-#calculate & output CA rmsf aligned w.r.t CA(t=0)#
-##################################################
+########################################################
+#rsmf: calculate & output CA rmsf aligned w.r.t CA(t=0)#
+########################################################
 
 set outfile [open "$NAME.rmsf" w]
 set sel [atomselect top "protein and name CA"]
@@ -94,9 +119,9 @@ for {set i 0} {\$i < [\$sel num]} {incr i} {
     }
 close \$outfile
 
-####################################
-#calculate & output rgyr of protein#
-####################################
+##########################################
+#rgyr: calculate & output rgyr of protein#
+##########################################
 
 set mol [molinfo top]
 set outfile [open "$NAME.rgyr" w]
@@ -111,46 +136,107 @@ for {set i 0} {\$i < \$frames} {incr i} {
     }
 close \$outfile
 
-##################################################################
-#calculate & output CA native contact number series w/ cutoff 4.5#
-##################################################################
+#######################################################################
+#pnc: calculate & output overall native contact fraction w/ cutoff 4.5#
+#######################################################################
 
-proc calc_nc {old_list new_list} {
-    set nc 0
-    foreach elem \$old_list {
-        if {\$elem in \$new_list} {
-            set nc [expr \$nc+1]
-        }
-    }
-    return \$nc
+source VMDextensions.tcl
+
+set mol [molinfo top]
+set outfile [open "$NAME.pnc" w]
+set frames [molinfo \$mol get numframes]
+set ca [atomselect top "protein and noh"]
+
+\$ca frame 0
+\$ca update
+set ref [ prepareNativeContacts 4.5 \$ca];
+set nnc [ llength \$ref]
+
+for {set i 0} {\$i < \$frames} {incr i 10} {
+    \$ca frame \$i
+    \$ca update
+    set nc [measureNativeContacts \$ref 4.5 \$ca]
+    set qnc [ expr 1.0 * \$nc / \$nnc ]
+    puts \$outfile  "\$i \$qnc"
 }
+close \$outfile
 
-set outfile [open "$NAME.nc" w]
-set nf [molinfo top get numframes]
-set ca [atomselect top "name CA"]
+##########################################################################
+#Recall the visualization state wo/ alignment and realign wrt. epitope CA#
+##########################################################################
 
-for {set f 0} {\$f < \$nf} {incr f 25} {
-    set ncf 0
-    foreach index [\$ca get index] {
-        set neighbor [atomselect top "(all within 4.5 of index \$index) and ({name \"C.*\"} or {name \"N.*\"} or {name \"O.*\"} and not {name \"OH.*\"})"]
+mol delete top
+play view_state.vmd
 
-        \$neighbor frame 0
-        \$neighbor update
-        set init_neighbor [\$neighbor get index]
+####################################
+#alignment CA w.r.t epitope CA(t=0)#
+####################################
 
-        \$neighbor frame \$f
-        \$neighbor update
-        set current_neighbor [\$neighbor get index]
+set ref [atomselect top "protein and name CA and (resid $epitope)" frame 0]
+set sel [atomselect top "protein and name CA and (resid $epitope)"]
+set all [atomselect top "all"]
+set n [molinfo top get numframes]
 
-        set nci [calc_nc \$init_neighbor \$current_neighbor]
-        set ncf [expr \$ncf+\$nci]
-	if {\$f==0} {set ncf0 [expr \$ncf+0.]}
+for {set i 1} {\$i < \$n} {incr i} {
+    \$sel frame \$i
+    \$all frame \$i
+    \$all move [measure fit \$sel \$ref]
     }
-    puts \$outfile  "\$f [expr \$ncf/\$ncf0]"
+
+#########################################################################
+#ermsd: calculate & output epitope CA rmsd aligned w.r.t epitope CA(t=0)#
+#########################################################################
+
+set outfile [open "$NAME.eermsd" w]
+set ref [atomselect top "protein and name CA and (resid $epitope)" frame 0]
+set sel [atomselect top "protein and name CA and (resid $epitope)"]
+for {set i 0} {\$i < \$n} {incr i} {
+  \$sel frame \$i
+  set rmsd [measure rmsd \$sel \$ref]
+  puts \$outfile "\$i \$rmsd"
+  }
+close \$outfile
+
+######################################################################################
+#ermsd-side: calculate & output epitope side chain rmsd aligned w.r.t epitope CA(t=0)#
+######################################################################################
+
+set outfile [open "$NAME.sermsd" w]
+set ref [atomselect top "protein and noh and not name CA and (resid $epitope)" frame 0]
+set sel [atomselect top "protein and noh and not name CA and (resid $epitope)"]
+for {set i 0} {\$i < \$n} {incr i} {
+  \$sel frame \$i
+  set rmsd [measure rmsd \$sel \$ref]
+  puts \$outfile "\$i \$rmsd"
+  }
+close \$outfile
+
+#######################################################################
+#enc: calculate & output epitope native contact fraction w/ cutoff 4.5#
+#######################################################################
+
+source VMDextensions.tcl
+
+set mol [molinfo top]
+set outfile [open "$NAME.enc" w]
+set frames [molinfo \$mol get numframes]
+set ca [atomselect top "protein and (resid $epitope) and noh"]
+
+\$ca frame 0
+\$ca update
+set ref [ prepareNativeContacts 4.5 \$ca];
+set nnc [ llength \$ref]
+
+for {set i 0} {\$i < \$frames} {incr i 10} {
+    \$ca frame \$i
+    \$ca update
+    set nc [measureNativeContacts \$ref 4.5 \$ca]
+    set qnc [ expr 1.0 * \$nc / \$nnc ]
+    puts \$outfile  "\$i \$qnc"
 }
 close \$outfile
 
 exit
 EOF
 
-eval "$VMD_EXE -e vmd.tcl";
+eval "$VMD_EXE -dispdev text -eofexit -e vmd.tcl >> vmd.log 2>&1";
